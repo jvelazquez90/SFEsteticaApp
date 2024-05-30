7ab4998a191908e0d0d4d8255c40ba18bb5ccc96
jvelazquez <jvelazquez.tab@gmail.com>
2024-05-30 12:27:27 -0300

    Se agrega archivo usuario.json y main.js para validar el login. Queda funcionando

---
+++ js/main.js
@@ -0,0 +1,34 @@
+document.getElementById("inicioSesion").addEventListener("submit", function(event) {
+  event.preventDefault(); // Evitar el envío del formulario
+
+  var username = document.getElementById("inputUsuario").value;
+  var password = document.getElementById("inputPassword").value;
+
+  // Realizar la solicitud Fetch para obtener los datos del JSON
+  fetch("./js/json/usuarios.json")
+  .then(response => {
+      if (!response.ok) {
+          throw new Error("No se pudo cargar el archivo JSON.");
+      }
+      return response.json();
+  })
+  .then(data => {
+      var usuarios = data.usuarios;
+
+      // Validar el usuario y la contraseña
+      var usuarioValido = usuarios.find(function(usuario) {
+          return usuario.usuario === username && usuario.contrasena === password;
+      });
+
+      if (usuarioValido) {
+          // Redireccionar a otra página si los datos son válidos
+          window.location.href = "./panel.html";
+      } else {
+          alert("Usuario o contraseña incorrectos");
+      }
+  })
+  .catch(error => {
+      console.error("Error:", error);
+      alert("Hubo un error al cargar los datos.");
+  });
+});
